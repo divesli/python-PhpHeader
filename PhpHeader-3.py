8c95e13798911f556efe57d9cb6e1f5e9260029e
divesli <divesli@gmail.com>
2018-04-05 22:42:56 +0800

    增加sublime3支持 增加文件夹右键菜单

---
+++ PhpHeader-3.py
@@ -0,0 +1,174 @@
+import sublime, sublime_plugin
+import datetime,time
+import os,getpass
+
+class PhpHeaderCommand(sublime_plugin.WindowCommand):
+    def run(self):
+        self.tab = self.window.new_file()
+        self.tab.window().show_input_panel("New file name:", "", self.do_input, None, self.do_cancel)
+        self.setSyntax()
+        self.tab.set_encoding("UTF-8")
+        #self.tab.run_command("second")
+        #sublime.set_timeout(lambda: self.tab.run_command("second"), 1000)
+
+    def do_input(self, txt):
+        try:
+            if len(txt.strip()) == 0:
+                txt = "Demo"
+            self.tab.set_name(txt+".php")
+            #sublime.set_timeout(lambda: self.tab.run_command("second"), 1000)
+            self.tab.run_command("second")
+        except Exception as e:
+            raise e
+
+    def do_cancel(self):
+        self.tab.set_name("Demo.php")
+
+    def setSyntax(self):
+        self.tab.set_syntax_file("Packages/PHP/PHP.tmLanguage")
+        self.tab.settings().set('default_extension', "php")
+
+class SecondCommand(sublime_plugin.TextCommand):
+    def run(self, edit):
+        try:
+            self.edit = edit
+            c = self.getCopyRight()
+            i = self.getDesc()
+            d = self.getDemo()
+            h = c + "\n\n" + i + "\n\n" + d
+            self.view.insert(self.edit, 0, h) 
+        except Exception as e:
+            raise e
+
+    def showMessage(self,txt):
+        sublime.message_dialog(txt)
+
+    def setCursor(self, view, edit): 
+        key = "@brief:"
+        region = sublime.Region(0,view.size())
+        point = view.substr(region).find(key) 
+        row,col = view.rowcol(point)    
+        pt = view.text_point(row,col+len(key)+1)
+
+        view.sel().clear()
+        view.sel().add(sublime.Region(pt))
+
+        view.show(pt)
+        #pass
+           
+
+    def getDemo(self):
+        fname = self.getFileName()
+        s = "."
+        try:
+            p = fname.index(s)
+            clname = fname[:p]
+        except:
+            clname = fname
+
+        if len(clname.strip()) == 0:
+            clname = "Demo"
+
+        demo =  "class " + clname + " {\n\n"
+        demo += "    /**\n"
+        demo += "     *\n"
+        demo += "     * @brief: construct functon\n"
+        demo += "     */\n"
+        demo += "    public function __construct() {\n"
+        demo += "        //TODO \n"
+        demo += "    }\n"
+        demo += "}\n"
+        return demo
+
+
+    def getCopyRight(self):
+        tmpl = "<?php\n"
+        tmpl += "/"
+        tmpl += "*" * 79
+        tmpl += "\n"
+        tmpl += " *\n"
+        tmpl += " * Copyright (c) "
+        tmpl += self.getYear() + " " + self.getDomain() +", Inc. All Rights Reserved\n"
+        tmpl += " * $Id$\n"
+        tmpl += " *\n"
+        tmpl += " "
+        tmpl += "*" * 79
+        tmpl += "/"
+        return tmpl
+
+    def getDesc(self):
+        desc = "/**\n"
+        desc += " * @file " + self.getFileName() + "\n"
+        desc += " * @author " + self.getUser() + "(" + self.getMail() + ")" + "\n"
+        desc += " * @date " + self.getTime() + "\n"
+        desc += " * @version " + self.getVersion() + "\n"
+        desc += " * @filecoding " + self.getFileCode() + "\n"
+        desc += " * @brief: \n"
+        desc += " *\n"
+        desc += " */"
+        return desc
+
+    def getDate(self, format = '%Y/%m/%d %H:%M:%I'):
+        t = time.time()
+        d = datetime.datetime.fromtimestamp(t)
+        return d.strftime(format)
+
+    def getYear(self):
+        y = self.getDate('%Y')
+        return y
+
+    def getFileName(self):
+        #return os.path.basename(self.view.file_name())
+        return self.view.name()
+
+    def getUser(self):
+        return getpass.getuser()
+
+    def getMail(self):
+        return self.getUser() + "@" + self.getDomain()
+
+    def getTime(self):
+        tm = self.getDate()
+        return tm
+
+    def getVersion(self):
+        return "$Revision 1.0$"
+
+    def getFileCode(self):
+        return "UTF-8"
+
+    def getDomain(self):
+        return "xxx.cn"
+
+
+class PhpDocsCommand(sublime_plugin.TextCommand):
+    def run(self, edit):
+        point = self.view.sel()[0].a
+        rowrgn = self.view.line(point)
+        row,col = self.view.rowcol(point)
+        region = sublime.Region(point, rowrgn.begin())
+        string = self.view.substr(region).strip()
+        docs = ''
+        if string == "/**":
+            docs = self.getDocs(col)
+        else:
+            self.view.run_command("insert",{"characters": "\n"})
+            return 
+            
+        pt = rowrgn.end()
+        self.view.insert(edit, pt, docs)
+
+        pxy = self.view.text_point(row+2,col+len("@brief: "))
+
+        self.view.sel().clear()
+        self.view.sel().add(sublime.Region(pxy))
+
+        self.view.show(pxy)
+
+    def getDocs(self,col):
+        space = " " * (col -2)
+        docs = "\n"
+        docs += space + "*\n"
+        docs += space + "* @brief: \n"
+        docs += space + "*/"
+        return docs 
